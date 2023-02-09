1235d83bc5f59f39755ebe6484e21c010da4d8b6
Alexander Sandberg <hi@alexandersandberg.com>
2023-02-09 08:46:29 +0100

    Initial Commit

---
+++ FocusIntentTest/FocusIntentTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusIntentTestApp.swift
+//  FocusIntentTest
+//
+//  Created by Alexander Sandberg on 09.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FocusIntentTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
